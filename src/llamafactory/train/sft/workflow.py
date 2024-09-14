dbe28f413dbe4081c269e527a9535740ffbbc542
Billy Cao <aliencaocao@gmail.com>
2024-09-14 22:07:04 +0800

    Add support for output scores in case evaluation metrics needs

--- src/llamafactory/train/sft/workflow.py
+++ src/llamafactory/train/sft/workflow.py
@@ -105,6 +105,8 @@ def run_sft(
     gen_kwargs["eos_token_id"] = [tokenizer.eos_token_id] + tokenizer.additional_special_tokens_ids
     gen_kwargs["pad_token_id"] = tokenizer.pad_token_id
     gen_kwargs["logits_processor"] = get_logits_processor()
+    gen_kwargs["output_scores"] = generating_args.output_scores
+    gen_kwargs["return_dict_in_generate"] = generating_args.output_scores
 
     # Training
     if training_args.do_train:
